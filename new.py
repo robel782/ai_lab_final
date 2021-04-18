cf53ee6e001fbce5e1948850008e0ee25c46af87
shaionbd <shakil.hossain@ucb.com.bd>
2021-04-18 17:25:48 +0600

    main file update

---
+++ new.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 10
+b = 20
+a + b
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+df = pd.read_csv('travel_agency_offers.csv')
+
+
+# In[8]:
+
+
+df.head(2)
+
+
+# In[ ]:
+
+
+
+
